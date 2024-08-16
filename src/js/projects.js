533bfd1738b76f6bb54fcda8e9bbb611a805de66
xd.faunredd <andrew.furevych@gmail.com>
2024-08-16 22:27:39 +0300

    update projects.css , html and js add svg

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,13 +1,18 @@
 import Swiper from 'swiper';
 import 'swiper/css';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Keyboard } from 'swiper/modules';
 
 const swiper = new Swiper('.swiper', {
-  modules: [Navigation],
+  modules: [Navigation, Keyboard],
   slidesPerView: 1,
   spaceBetween: 30,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
   },
 });
