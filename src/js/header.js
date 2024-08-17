0b68944de79ea64f8c8f8e0227fe83ccfeb0ab81
lehenkyi-yevhen-ar <meister.ramires@gmail.com>
2024-08-17 18:08:44 +0300

    add header and modal

---
+++ src/js/header.js
@@ -0,0 +1,7 @@
+// document.querySelector('.header-modal-btn').addEventListener('click', function() {
+//     document.querySelector('.header-modal-window').style.display = 'flex';
+//   });
+  
+//   document.querySelector('.header-modal-close').addEventListener('click', function() {
+//     document.querySelector('.header-modal-window').style.display = 'none';
+//   });
