2109b408e31b06b0caf32a2a7b51e1721d47351e
Iryna Kolomiichenko <159377221+Irene0850@users.noreply.github.com>
2024-08-18 14:16:14 +0300

    new now

--- src/js/about.js
+++ src/js/about.js
@@ -12,7 +12,7 @@ aboute.forEach(about => {
   });
 });
 
-const swiperAboute = new Swiper('.swiper-wrapper', {
+const swiperAboute = new Swiper('.swiper-container', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
