96c9113b3789731f807d8c86bbb4cd6351059549
Yarik2019 <yroslavterno@gmail.com>
2024-08-18 21:27:13 +0300

    fixed arrow

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -9,39 +9,69 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const swiper = new Swiper('.swiper', {
   modules: [Navigation],
-  // breakpoints: {
-  //   // when window width is >= 320
-  //   320: {
-  //     slidesPerView: 6,
-  //     spaceBetween: 20,
-  //   },
-  //   // when window width is >= 768
-  //   768: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 30,
-  //   },
-  //   // when window width is >= 1440
-  //   1440: {
-  //     slidesPerView: 4,
-  //     spaceBetween: 40,
-  //   },
-  // },
-  slidesPerView: 'auto',
-  spaceBetween: 30,
+  breakpoints: {
+    // when window width is >= 320
+    320: {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+    // when window width is >= 768
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 1440
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.review-btn-next',
+    prevEl: '.review-btn-prev',
   },
   keyboard: {
     enabled: true,
-    onlyInViewport: false,
+    onlyInViewport: true,
+    pageUpDown: true,
   },
-  a11y: {
-    prevSlideMessage: 'Previous slide',
-    nextSlideMessage: 'Next slide',
-  },
-  grabCursor: true,
 });
+
+const reviewBtnPrev = document.querySelector(
+  '.review-btn-container .review-btn-prev'
+);
+
+const reviewBtnNext = document.querySelector(
+  '.review-btn-container .review-btn-next'
+);
+
+const reviewHiddenPrev = document.querySelector('.review-hidden-prev');
+const reviewHiddenNext = document.querySelector('.review-hidden-next');
+
+function checkStatus() {
+  reviewHiddenNext.style.display = reviewBtnNext.hasAttribute('disabled')
+    ? 'block'
+    : 'none';
+  reviewBtnNext.firstElementChild.style.display = reviewBtnNext.hasAttribute(
+    'disabled'
+  )
+    ? 'none'
+    : 'block';
+
+  reviewHiddenPrev.style.display = reviewBtnPrev.hasAttribute('disabled')
+    ? 'block'
+    : 'none';
+  reviewBtnPrev.firstElementChild.style.display = reviewBtnPrev.hasAttribute(
+    'disabled'
+  )
+    ? 'none'
+    : 'block';
+}
+checkStatus();
+
+reviewBtnNext.addEventListener('click', checkStatus);
+reviewBtnPrev.addEventListener('click', checkStatus);
 
 const ulElement = document.querySelector('.js-list-reviews');
 
