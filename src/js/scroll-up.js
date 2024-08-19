884789f651667c6cbc227b1787a86598d4fdae7e
Mrszlv <miroszlav.popovics@gmail.com>
2024-08-19 09:52:19 +0300

    scroll-btn

--- src/js/scroll-up.js
+++ src/js/scroll-up.js
@@ -1,1 +1,23 @@
 // scroll
+
+const scrollUpBtn = document.querySelector('.scroll-up-btn');
+scrollUpBtn.addEventListener('click', scrollToTop);
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'auto',
+    });
+}
+
+function showScrollToTopBtn() {
+    if (window.scrollY > 1000) {
+        scrollUpBtn.classList.remove('scroll-up-btn-hidden');
+    } else {
+        scrollUpBtn.classList.add('scroll-up-btn-hidden');
+    }
+}
+showScrollToTopBtn();
+
+window.addEventListener('scroll', scrollUpBtn);
+
