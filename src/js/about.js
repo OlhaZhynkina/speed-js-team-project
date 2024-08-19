7d2dfbd978da1ffb9207ca683becd3396292d962
Iryna Kolomiichenko <159377221+Irene0850@users.noreply.github.com>
2024-08-19 13:42:12 +0300

    about js

--- src/js/about.js
+++ src/js/about.js
@@ -12,26 +12,6 @@ const swiperAbout = new Swiper('.swiper-container', {
   pagination: {
     el: '.swiper-pagination',
   },
-  modules: [Navigation],
-  slidesPerView: 2,
-  spaceBetween: 1,
-  breakpoints: {
-    // when window width is >= 320
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 1,
-    },
-    // when window width is >= 768
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 1,
-    },
-    // when window width is >= 1440
-    1440: {
-      slidesPerView: 6,
-      spaceBetween: 1,
-    },
-  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -47,5 +27,3 @@ const swiperAbout = new Swiper('.swiper-container', {
   },
   modules: [Navigation, Keyboard, Mousewheel],
 });
-
-$('.swiper-slide-p').slick({});
